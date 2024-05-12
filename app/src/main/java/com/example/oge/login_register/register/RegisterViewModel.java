1c2177758bc0117d22c4dff85af3fb7583b3aceb
JarikVI <jarikmotorin@gmail.com>
2024-05-12 19:34:25 +0200

    first commit

---
+++ app/src/main/java/com/example/oge/login_register/register/RegisterViewModel.java
@@ -0,0 +1,7 @@
+package com.example.oge.login_register.register;
+
+import androidx.lifecycle.ViewModel;
+
+public class RegisterViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
